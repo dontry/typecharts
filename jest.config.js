ecb44b7b2d2f874c142eac5201fdfe58e80b7122
dontry <mccoy018@gmail.com>
2020-04-03 21:43:30 +1100

    build(npm): improve scaffolding

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,21 @@
+/* eslint-disable no-undef */
 //jest-environment-jsdom is browser env containing a window object
 module.exports = {
   preset: "ts-jest",
-  collectCoverageFrom: ["**/lib/**/*.ts"],
+  collectCoverageFrom: ["<rootDir>/lib/**/*.ts"],
   testPathIgnorePatterns: ["<rootDir>/node_modules/"],
+  modulePathIgnorePatterns: ["<rootDir>/node_modules/"],
+  moduleNameMapper: {
+    "^@/types/(.*)$": "<rootDir>/lib/types/$1",
+    "^@/components/(.*)$": "<rootDir>/lib/components/$1",
+    "^@/utils/(.*)$": "<rootDir>/lib/utils/$1",
+  },
   coverageThreshold: {
     global: {
       statements: 10,
       branches: 4,
       lines: 10,
-      functions: 10
-    }
-  }
+      functions: 10,
+    },
+  },
 };
