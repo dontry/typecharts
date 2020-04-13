f7f5bd044fd68fd36812af041211fccda0499ddc
dontry <mccoy018@gmail.com>
2020-04-13 14:53:18 +1000

    test(fixtures): add parseCSVfile util

--- src/tests/fixtures/utils.ts
+++ src/tests/fixtures/utils.ts
@@ -2,11 +2,12 @@ import { parse } from "papaparse";
 import fs from "fs";
 import { DataItem } from "@/types/DataItem";
 
-export function parseCsvData(filePath: string): DataItem[] {
+export function parseCsvData(filePath: string, fastMode?: boolean): DataItem[] {
   const file = fs.readFileSync(filePath, "utf8");
   const res = parse(file, {
+    fastMode: fastMode,
     delimiter: ",",
-    fastMode: true,
+    quoteChar: '"',
     header: true,
   });
 
