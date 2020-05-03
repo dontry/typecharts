a95eb20b8f4c48833f64ccf8b269ef74e861ccef
dontry <mccoy018@gmail.com>
2020-05-03 19:54:05 +1000

    feat(chart): export charts

---
+++ src/charts/index.ts
@@ -0,0 +1,5 @@
+export { BarChart } from "./Bar/BarChart";
+export { PieChart } from "./Pie/PieChart";
+export { ScatterChart } from "./Scatter/ScatterChart";
+export { LineChart } from "./Line/LineChart";
+export { ChartFactory } from "./ChartFactory";
